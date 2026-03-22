ad989e201328644bd8c7fa4e5122ccbcc00e6390
unknown <Admin@DESKTOP-V8H3KC1.Mahavitaran.in>
2026-03-22 22:25:35 +0530

    Revert "commit to revert"
    This reverts commit 140271c9e86c93c4d1d1fd7794bd0c54644d5a2a.

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,6 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  timeout: 30000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   timeout: 10000,
